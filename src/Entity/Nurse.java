848aba9e7ffa64a0d46a00d21c7d9467f3225453
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-10 12:22:12 +0400

    Add specific fields for Nurse class

--- src/Entity/Nurse.java
+++ src/Entity/Nurse.java
@@ -1,4 +1,12 @@
 package Entity;
 
-public class Nurse {
+import java.util.List;
+
+public class Nurse extends Person {
+
+    private String nurseId;
+    private String departmentId;
+    private String shift; // Morning / Evening / Night
+    private String qualification;
+    private List<String> assignedPatients;
 }
