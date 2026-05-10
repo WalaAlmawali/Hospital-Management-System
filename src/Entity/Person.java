e2996d9b7ceaab5217774d375e497c911e804e54
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-10 09:51:32 +0400

    Add attripute for Person class

--- src/Entity/Person.java
+++ src/Entity/Person.java
@@ -4,5 +4,11 @@ import java.util.Date;
 import java.util.Objects;
 
 public class Person {
-
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String address;
 }
