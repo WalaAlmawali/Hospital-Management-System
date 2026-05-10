60f74f7820d241e784f89f88f59c995b20a2d541
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-10 12:23:39 +0400

    Implement constructor chaining for Nurse class

--- src/Entity/Nurse.java
+++ src/Entity/Nurse.java
@@ -1,12 +1,27 @@
 package Entity;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Nurse extends Person {
 
+    // Nurse-specific fields
     private String nurseId;
     private String departmentId;
     private String shift; // Morning / Evening / Night
     private String qualification;
     private List<String> assignedPatients;
+
+
+    // Constructor with constructor chaining
+    public Nurse(String id, String firstName, LocalDate dateOfBirth, String lastName, String gender, String phoneNumber, String email, String address, String nurseId, String departmentId, String shift, String qualification, List<String> assignedPatients) {
+        super(id, firstName, dateOfBirth, lastName, gender, phoneNumber, email, address);
+        this.nurseId = nurseId;
+        this.departmentId = departmentId;
+        this.shift = shift;
+        this.qualification = qualification;
+        this.assignedPatients = new ArrayList<>();
+    }
 }
+
