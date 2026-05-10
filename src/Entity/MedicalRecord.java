1797629f5bf2f20f9e3a5c51b9b64ad379e15fb1
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-10 11:43:57 +0400

    Add Fields of MedicalRecord class

--- src/Entity/MedicalRecord.java
+++ src/Entity/MedicalRecord.java
@@ -1,4 +1,14 @@
 package Entity;
 
+import java.time.LocalDate;
+
 public class MedicalRecord {
+    private String recordId;
+    private String patientId;
+    private String doctorId;
+    private LocalDate visitDate;
+    private String diagnosis;
+    private String prescription;
+    private String testResults;
+    private String notes;
 }
