9cb9e438eb09eed7f5bdfb321112da3498cccdc5
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-10 15:26:00 +0400

    Declare list of doctors

--- src/Service/DoctorService.java
+++ src/Service/DoctorService.java
@@ -1,4 +1,12 @@
 package Service;
 
+import Entity.Doctor;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DoctorService {
+
+    static List<Doctor> doctors = new ArrayList<>();
+
 }
