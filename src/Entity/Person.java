17efb9f8de95efe5aa5b76e8eb956f43e7e3b083
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-10 09:52:27 +0400

    Implement full constructor

--- src/Entity/Person.java
+++ src/Entity/Person.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.Objects;
 
 public class Person {
+    private String id;
     private String firstName;
     private String lastName;
     private Date dateOfBirth;
@@ -11,4 +12,15 @@ public class Person {
     private String phoneNumber;
     private String email;
     private String address;
+
+    public Person(String id, String firstName, Date dateOfBirth, String lastName, String gender, String phoneNumber, String email, String address) {
+        this.id = id;
+        this.firstName = firstName;
+        this.dateOfBirth = dateOfBirth;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+    }
 }
