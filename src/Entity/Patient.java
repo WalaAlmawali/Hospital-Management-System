7d19a3f5fe49c0431d49f178d8a91867fe34a160
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-10 10:11:32 +0400

    Override displayInfo() to include patient-specific details

--- src/Entity/Patient.java
+++ src/Entity/Patient.java
@@ -18,4 +18,20 @@ public class Patient extends Person {
     public Patient(String id, String firstName, Date dateOfBirth, String lastName, String gender, String phoneNumber, String email, String address) {
         super(id, firstName, dateOfBirth, lastName, gender, phoneNumber, email, address);
     }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("patient Id : "+ patientId);
+        System.out.println("patient blood Group : "+ bloodGroup);
+        System.out.println("patient allergies : "+ allergies);
+        System.out.println("patient emergency Contact : "+ emergencyContact);
+        System.out.println("patient registration Date : "+ registrationDate);
+        System.out.println("patient insuranceId : "+ insuranceId);
+        System.out.println("patient medical Records : "+ medicalRecords);
+        System.out.println("patient appointments : "+ appointments);
+
+    }
+
+
 }
