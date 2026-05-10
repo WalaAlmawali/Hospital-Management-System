dd08e4d36fa5f0f399a09454383d153065ca516b
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-10 10:04:11 +0400

    Add specific fields to Patient

--- src/Entity/Patient.java
+++ src/Entity/Patient.java
@@ -1,4 +1,17 @@
 package Entity;
 
-public class Patient {
+import javax.xml.crypto.Data;
+import java.util.List;
+
+public class Patient extends Person {
+
+    private String patientId;
+    private String bloodGroup;
+    private List allergies;
+    private String emergencyContact;
+    private Data registrationDate;
+    private String insuranceId;
+    private List medicalRecords;
+    private List appointments;
+
 }
