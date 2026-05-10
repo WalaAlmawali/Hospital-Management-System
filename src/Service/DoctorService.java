afc6db8bdd473672cc070af0f7b0913e10cb9549
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-10 15:29:11 +0400

    Import Scanner + Create object of class Scanner

--- src/Service/DoctorService.java
+++ src/Service/DoctorService.java
@@ -2,11 +2,18 @@ package Service;
 
 import Entity.Doctor;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class DoctorService {
+    Scanner scanner = new Scanner(System.in);
 
     static List<Doctor> doctors = new ArrayList<>();
+
+    public Doctor addDoctor(){
+
+    }
 
 }
