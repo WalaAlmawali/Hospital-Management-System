a69ae5954dad8f3c81c327b6d0657d26467ccc99
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-11 14:05:01 +0400

    Add full constructor for InPatient class

--- src/Entity/InPatient.java
+++ src/Entity/InPatient.java
@@ -1,6 +1,7 @@
 package Entity;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class InPatient extends Patient{
 
@@ -10,5 +11,15 @@ public class InPatient extends Patient{
     private String bedNumber;
     private String admittingDoctorId;
     private double dailyCharges;
+
+    public InPatient(String id, String firstName, LocalDate dateOfBirth, String lastName, String gender, String phoneNumber, String email, String address, String patientId, String bloodGroup, List<String> allergies, String emergencyContact, LocalDate registrationDate, List<MedicalRecord> medicalRecords, String insuranceId, List<Appointment> appointments, LocalDate admissionDate, LocalDate dischargeDate, String roomNumber, String bedNumber, String admittingDoctorId, double dailyCharges) {
+        super(id, firstName, dateOfBirth, lastName, gender, phoneNumber, email, address, patientId, bloodGroup, allergies, emergencyContact, registrationDate, medicalRecords, insuranceId, appointments);
+        this.admissionDate = admissionDate;
+        this.dischargeDate = dischargeDate;
+        this.roomNumber = roomNumber;
+        this.bedNumber = bedNumber;
+        this.admittingDoctorId = admittingDoctorId;
+        this.dailyCharges = dailyCharges;
+    }
 
 }
