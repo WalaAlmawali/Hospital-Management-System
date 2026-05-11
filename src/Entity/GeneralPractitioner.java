b6301f65a9f452c20dfc4c412288c008eb7143d9
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-11 13:53:49 +0400

    Add getter and setter for all fields

--- src/Entity/GeneralPractitioner.java
+++ src/Entity/GeneralPractitioner.java
@@ -15,4 +15,28 @@ public class GeneralPractitioner extends Doctor {
         this.homeVisitAvailable = homeVisitAvailable;
         this.vaccinationCertified = vaccinationCertified;
     }
+
+    public boolean isWalkinAvailable() {
+        return walkinAvailable;
+    }
+
+    public void setWalkinAvailable(boolean walkinAvailable) {
+        this.walkinAvailable = walkinAvailable;
+    }
+
+    public boolean isVaccinationCertified() {
+        return vaccinationCertified;
+    }
+
+    public void setVaccinationCertified(boolean vaccinationCertified) {
+        this.vaccinationCertified = vaccinationCertified;
+    }
+
+    public boolean isHomeVisitAvailable() {
+        return homeVisitAvailable;
+    }
+
+    public void setHomeVisitAvailable(boolean homeVisitAvailable) {
+        this.homeVisitAvailable = homeVisitAvailable;
+    }
 }
