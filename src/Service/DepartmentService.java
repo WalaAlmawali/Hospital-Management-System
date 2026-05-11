648d8abe130cb2dcca9b9af1bf3bd2e04417b2e3
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-11 11:04:08 +0400

    Complete addDepartment function

--- src/Service/DepartmentService.java
+++ src/Service/DepartmentService.java
@@ -6,8 +6,11 @@ import Entity.Nurse;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class DepartmentService {
+
+    Scanner scanner = new Scanner(System.in);
 
     static List<Department> departmentList = new ArrayList<>();
     List<Doctor> doctors = new ArrayList<>();
@@ -15,9 +18,24 @@ public class DepartmentService {
 
     public Department addDepartment(){
 
+        System.out.println("Enter Department Id :");
+        String departmentId = scanner.nextLine();
 
+        System.out.println("Enter department Name :");
+        String departmentName = scanner.nextLine();
 
+        System.out.println("Enter department head DoctorId :");
+        String headDoctorId = scanner.nextLine();
 
+        System.out.println("Enter department  bed Capacity :");
+        int bedCapacity = scanner.nextInt();
+
+        System.out.println("Enter department available Beds :");
+        int availableBeds = scanner.nextInt();
+
+        Department department = new Department(departmentId,departmentName,headDoctorId,doctors,nurses,bedCapacity,availableBeds);
+
+        return department;
 
     }
 
