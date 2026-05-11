44a17a3185968faf2afc032b1c7f441f15a05f90
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-11 08:06:39 +0400

    Create a static list named nurseList

--- src/Service/NurseService.java
+++ src/Service/NurseService.java
@@ -1,9 +1,15 @@
 package Service;
 
+import Entity.Nurse;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class NurseService {
     Scanner scanner = new Scanner(System.in);
+
+    static List<Nurse> nurseList = new ArrayList<>();
 
 
 }
