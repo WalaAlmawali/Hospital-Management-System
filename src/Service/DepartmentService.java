df8fe9cdc4479edb452bc17eff2ce1cf40390d25
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-11 10:54:16 +0400

    Create new static list named departmentList

--- src/Service/DepartmentService.java
+++ src/Service/DepartmentService.java
@@ -1,6 +1,12 @@
 package Service;
 
+import Entity.Department;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DepartmentService {
 
+    static List<Department> departmentList = new ArrayList<>();
 
 }
