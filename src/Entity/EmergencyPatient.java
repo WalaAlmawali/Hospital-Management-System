41e8d390b27c409a689d070984052355431e6b6a
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-11 14:40:04 +0400

    Add Additional fields for EmergencyPatient class

--- src/Entity/EmergencyPatient.java
+++ src/Entity/EmergencyPatient.java
@@ -1,4 +1,11 @@
 package Entity;
 
 public class EmergencyPatient extends Patient{
+
+    private String emergencyType;
+    private String arrivalMode; // Ambulance / Walk-in
+    private int triageLevel;    // 1 to 5
+    private boolean admittedViaER;
+
+
 }
