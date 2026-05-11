394373d7497f5359ac95974b95a57d84ce63be3d
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-11 13:53:27 +0400

    Add constructor for  GeneralPractitioner clas

--- src/Entity/GeneralPractitioner.java
+++ src/Entity/GeneralPractitioner.java
@@ -1,10 +1,18 @@
 package Entity;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class GeneralPractitioner extends Doctor {
 
     private boolean walkinAvailable;
     private boolean homeVisitAvailable;
     private boolean vaccinationCertified;
 
-
+    public GeneralPractitioner(String id, String firstName, LocalDate dateOfBirth, String lastName, String gender, String phoneNumber, String email, String address, String doctorId, String specialization, String qualification, int experienceYears, String departmentId, double consultationFee, List<String> availableSlots, List<String> assignedPatients, boolean walkinAvailable, boolean homeVisitAvailable, boolean vaccinationCertified) {
+        super(id, firstName, dateOfBirth, lastName, gender, phoneNumber, email, address, doctorId, specialization, qualification, experienceYears, departmentId, consultationFee, availableSlots, assignedPatients);
+        this.walkinAvailable = walkinAvailable;
+        this.homeVisitAvailable = homeVisitAvailable;
+        this.vaccinationCertified = vaccinationCertified;
+    }
 }
