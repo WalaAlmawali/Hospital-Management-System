ec20b039405f63b3edfeb53f6a3604507a574836
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-11 09:22:03 +0400

    Create object of MedicalRecord class + add return part of addMedicalRecord function

--- src/Service/MedicalRecordService.java
+++ src/Service/MedicalRecordService.java
@@ -3,6 +3,7 @@ package Service;
 import Entity.MedicalRecord;
 import Entity.Nurse;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -14,6 +15,33 @@ public class MedicalRecordService {
 
     public MedicalRecord addMedicalRecord() {
 
+        System.out.println("Enter record Id :");
+        String recordId = scanner.nextLine();
 
+        System.out.println("Enter patient Id :");
+        String patientId = scanner.nextLine();
+
+        System.out.println("Enter doctor Id :");
+        String doctorId = scanner.nextLine();
+
+        System.out.println("Enter visit Date:");
+        String visitDate = scanner.nextLine();
+        LocalDate date = LocalDate.parse(visitDate);
+
+        System.out.println("Enter diagnosis :");
+        String diagnosis = scanner.nextLine();
+
+        System.out.println("Enter prescription :");
+        String prescription = scanner.nextLine();
+
+        System.out.println("Enter test Results :");
+        String testResults = scanner.nextLine();
+
+        System.out.println("Enter notes :");
+        String notes = scanner.nextLine();
+
+        MedicalRecord medicalRecord = new MedicalRecord(recordId,patientId,doctorId,date,diagnosis,testResults,prescription,notes);
+
+        return medicalRecord;
     }
 }
