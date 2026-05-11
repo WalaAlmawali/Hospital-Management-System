b6eb85f5dbef9ea5bb6a197198ba63c53354774a
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-11 14:25:44 +0400

    Add Additional fields fo OutPatient class

--- src/Entity/OutPatient.java
+++ src/Entity/OutPatient.java
@@ -1,5 +1,12 @@
 package Entity;
 
+import java.time.LocalDate;
+
 public class OutPatient extends Patient{
+
+    private int visitCount;
+    private LocalDate lastVisitDate;
+    private String preferredDoctorId;
+
 
 }
