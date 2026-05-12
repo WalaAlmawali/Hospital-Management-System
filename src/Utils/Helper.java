50b39c1b972f03f6ad2ee6ca65e7af27adde6c2c
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-12 12:16:07 +0400

    Implement isValidString(String str)+ isValidString(String str, int minLength) functions

--- src/Utils/Helper.java
+++ src/Utils/Helper.java
@@ -2,6 +2,8 @@ package Utils;
 
 public class Helper {
 
+    // *****Null Check Methods (Overloaded)*****
+
     // Check if Object is null
     public static boolean isNull(Object obj) {
         return obj == null;
@@ -21,5 +23,24 @@ public class Helper {
     public static boolean isNotNull(String str) {
         return str != null && !str.trim().isEmpty();
     }
+
+    // *****String Validation Methods (Overloaded) *****
+
+    //  Check not null and not empty
+    public static boolean isValidString(String str) {
+        return str != null && !str.trim().isEmpty();
+    }
+
+    //  Check minimum length
+
+    public static boolean isValidString(String str, int minLength) {
+
+        if (!isValidString(str)) {
+            return false;
+        }
+
+        return str.trim().length() >= minLength;
+    }
+
 
 }
